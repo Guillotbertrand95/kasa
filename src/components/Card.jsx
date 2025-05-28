ced19acf2230f14411017fed39c5fbaf1c04fb1b
guillot bertrand <guillotbertrand95@gmail.com>
2025-05-28 11:26:25 +0200

    page home

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,14 +1,17 @@
 import { Link } from "react-router-dom";
+import "../styles/card.scss";
 
 function Card({ logement }) {
 	return (
 		<Link to={`/apartment/${logement.id}`} className="card">
-			<img
-				src={logement.cover}
-				alt={logement.title}
-				className="image-appart"
-			/>
-			<h3>{logement.title}</h3>
+			<div className="card-image-container">
+				<img
+					src={logement.cover}
+					alt={logement.title}
+					className="image-appart"
+				/>
+				<h3 className="card-title">{logement.title}</h3>
+			</div>
 		</Link>
 	);
 }
